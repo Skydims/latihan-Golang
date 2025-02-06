cb3505dda4e20818801a8278fd27da953214a5bf
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-06 23:09:00 +0700

    Update main.go

--- 08_pointers/03_using-pointers/main.go
+++ 08_pointers/03_using-pointers/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-	a := 43
+	a := 24
 
-	fmt.Println(a)  // 43
+	fmt.Println(a)  // 24
 	fmt.Println(&a) // 0x20818a220
 
 	var b = &a
 	fmt.Println(b)  // 0x20818a220
-	fmt.Println(*b) // 43
+	fmt.Println(*b) // 24
 
 	*b = 42        // b says, "The value at this address, change it to 42"
 	fmt.Println(a) // 42
