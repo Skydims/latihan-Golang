0fff1af71f7a0a8f15a1da771f133bf7d88328a5
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-06 23:07:22 +0700

    Update main.go

--- 07_memory-address/02_using-address/main.go
+++ 07_memory-address/02_using-address/main.go
@@ -6,8 +6,8 @@ const metersToYards float64 = 1.09361
 
 func main() {
 	var meters float64
-	fmt.Print("Enter meters swam: ")
+	fmt.Print("masuukan meteran swam: ")
 	fmt.Scan(&meters)
 	yards := meters * metersToYards
-	fmt.Println(meters, " meters is ", yards, " yards.")
+	fmt.Println(meters, " meters yaitu ", yards, " yards.")
 }
