39b058d51fec13f3fec90b097b043cb433abd37d
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-07 23:03:44 +0700

    Update main.go

--- 10_for-loop/02_nested/main.go
+++ 10_for-loop/02_nested/main.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 12; j++ {
 			fmt.Println(i, " - ", j)
 		}
 	}
