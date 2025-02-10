9249766a2cc9e0751148f71becbb51a5caba4298
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-10 22:59:02 +0700

    Update main.go

--- 16_exercise-solutions/03_variadic-greatest/main.go
+++ 16_exercise-solutions/03_variadic-greatest/main.go
@@ -13,7 +13,7 @@ func max(numbers ...int) int {
 }
 
 func main() {
-	greatest := max(4, 7, 12, 123, 543, 24, 435, 53, 125)
+	greatest := max(4, 7, 12, 123, 543, 24, 45, 53, 125)
 	fmt.Println(greatest)
 }
 
