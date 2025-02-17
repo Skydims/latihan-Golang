4d2d12eeda2ef531868224a066672add82080490
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-17 22:35:51 +0700

    Update main.go

--- 20_struct/02_struct_fields_values_initialization/main.go
+++ 20_struct/02_struct_fields_values_initialization/main.go
@@ -10,7 +10,7 @@ type person struct {
 
 func main() {
 	p1 := person{"Dimas", "adi", 20}
-	p2 := person{"sarah", "julian sari", 18}
+	p2 := person{"sarah", "julian sari", 21}
 	fmt.Println(p1.first, p1.last, p1.age)
 	fmt.Println(p2.first, p2.last, p2.age)
 }
