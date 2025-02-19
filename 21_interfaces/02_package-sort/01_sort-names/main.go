abed00dd71403b99ba6acc1de0a8602c8ecaca2b
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-19 21:35:28 +0700

    Update main.go

--- 21_interfaces/02_package-sort/01_sort-names/main.go
+++ 21_interfaces/02_package-sort/01_sort-names/main.go
@@ -12,7 +12,7 @@ func (p people) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p people) Less(i, j int) bool { return p[i] < p[j] }
 
 func main() {
-	studyGroup := people{"dimas", "Sarah", "azzam", "Jenny"}
+	studyGroup := people{"dimas", "Sarah", "azzam", "Raya"}
 
 	fmt.Println(studyGroup)
 	sort.Sort(studyGroup)
