f45cf78da77c0a032d80630ccd213f345d95ddcd
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-19 21:34:33 +0700

    Update main.go

--- 21_interfaces/01_interface/05_io-copy/01_no-error-checking/main.go
+++ 21_interfaces/01_interface/05_io-copy/01_no-error-checking/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	msg := "Do not dwell in the past, do not dream of the future, concentrate the mind on the present."
+	msg := "Do not well in the past, do not dream of the future, concentrate the mind on the present."
 	rdr := strings.NewReader(msg)
 	io.Copy(os.Stdout, rdr)
 
