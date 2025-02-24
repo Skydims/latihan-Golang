7835a28bfabc536f6cc6ebe785752c90a1491858
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-24 22:46:08 +0700

    Update main.go

--- 23_error-handling/02_err-not-nil/02_log-println/main.go
+++ 23_error-handling/02_err-not-nil/02_log-println/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	_, err := os.Open("no-file.txt")
+	_, err := os.Open("noo-file.txt")
 	if err != nil {
 		//		fmt.Println("err happened", err)
 		log.Println("err happened", err)
