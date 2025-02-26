e67ae888b8178a908bb1d53a217a69436ebdbff0
DIMAS ADI SAPUTRA <110624244+Skydims@users.noreply.github.com>
2025-02-26 22:23:43 +0700

    Update main.go

--- 10_for-loop/07_rune-loop_UTF8/01/main.go
+++ 10_for-loop/07_rune-loop_UTF8/01/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 250; i <= 240; i++ {
+	for i := 25; i <= 240; i++ {
 		fmt.Println(i, " - ", string(i), " - ", []byte(string(i)))
 	}
 	foo := "a"
